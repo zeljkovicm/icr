8bc84542998692829c2ead1467c6fb1f390118c9
zeljkovicm <zeljkovic_milan@yahoo.com>
2025-11-27 11:27:38 +0100

    Added search logic

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,17 +2,28 @@ import { Component, signal } from '@angular/core';
 import { MovieModel } from '../models/movie.model';
 import { MovieService } from '../services/movie.service';
 import { RouterLinkWithHref } from "@angular/router";
+import { ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms"
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, ɵInternalFormsSharedModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   protected movies = signal<MovieModel[]>([])
-
+  protected previousSerach = 'N/A'
+  protected search = ''
   constructor() {
-    MovieService.getMovies().then(rsp => this.movies.set(rsp.data))
+    this.loadMovies()
+  }
+
+  protected loadMovies() {
+    if (this.previousSerach == '' && this.search == '')
+      return
+
+    this.previousSerach = this.search
+    MovieService.getMovies(this.search).then(rsp => this.movies.set(rsp.data))
   }
 }
