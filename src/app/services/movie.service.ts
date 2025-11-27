e3384345febf43ed9a6675110e9ef824cccaafd8
zeljkovicm <zeljkovic_milan@yahoo.com>
2025-11-27 11:28:17 +0100

    Modified constructor to be able to receive search parameter

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class MovieService {
-    static async getMovies() {
-        return axios.get('https://movie.pequla.com/api/movie')
+    static async getMovies(search: string = '') {
+        return axios.get(`https://movie.pequla.com/api/movie?search=${search}`)
     }
 }
