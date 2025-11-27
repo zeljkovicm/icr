59ee9e28a71125345b4153d0573b17f947975d31
zeljkovicm <zeljkovic_milan@yahoo.com>
2025-11-27 08:55:30 +0100

    Added layout for home page depending on movie API data

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { MovieModel } from '../models/movie.model';
 import { MovieService } from '../services/movie.service';
+import { RouterLinkWithHref } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLinkWithHref],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  protected movies = signal<any>(null)
+  protected movies = signal<MovieModel[]>([])
 
   constructor() {
-    MovieService.getMovies().then(rsp => this.movies.set(JSON.stringify(rsp.data, null, 2)))
+    MovieService.getMovies().then(rsp => this.movies.set(rsp.data))
   }
 }
